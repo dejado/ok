0bd9acfe9f3c5a90b13a6aceeacc6dcd3014350b
nhjin94 <nhjin94@gmail.com>
2024-02-18 11:49:09 +0900

    생산메인

--- SuJinChemicalMES/Properties/Resources.Designer.cs
+++ SuJinChemicalMES/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace SuJinChemicalMES.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap tamk02 {
+            get {
+                object obj = ResourceManager.GetObject("tamk02", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
