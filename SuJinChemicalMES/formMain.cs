8ef38cd1e1f96e5c421d1f3b49ece6e4f7d065b2
dejado <seotoo75@kangwon.ac.kr>
2024-02-19 17:03:16 +0900

    메인

--- SuJinChemicalMES/formMain.cs
+++ SuJinChemicalMES/formMain.cs
@@ -30,6 +30,8 @@ namespace SuJinChemicalMES
             AddCalendar();
             // 폼이 생성될 때 캘린더 호출
             AddChart();
+            LoadImageFromDatabase();
+            InitializeTimer();
         }
 
         DateTime base_day;
@@ -228,6 +230,101 @@ namespace SuJinChemicalMES
         private void MainMoni_lb_Click(object sender, EventArgs e)
         {
 
+        }
+        private Timer timer;
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 100; // 5초 간격으로 설정 (원하는 간격으로 수정 가능)
+            timer.Tick += timer1_Tick_1;
+            timer.Start();
+        }
+
+        public void LoadImageFromDatabase()
+        {
+            List<PictureBox> pictureBoxList = new List<PictureBox>
+        {pictureBox5, pictureBox4, pictureBox3, pictureBox2, pictureBox1, pictureBox6 };
+            string connectionString = "Server=10.10.32.82;Database=production_management;Uid=team;Pwd=team1234;";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT bath_num, medicine_type FROM bath";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string bethNumber = reader["bath_num"].ToString();
+                            string chemicalType = reader["medicine_type"].ToString();
+
+                            int secondCharacterAsInt = 0;
+
+                            if (!string.IsNullOrEmpty(bethNumber) && bethNumber.Length >= 2)
+                            {
+                                char secondCharacter = bethNumber[2];
+
+                                if (int.TryParse(secondCharacter.ToString(), out int result))
+                                {
+                                    secondCharacterAsInt = result;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("변환 실패");
+                                }
+                            }
+
+                            SetPictureBoxImage(pictureBoxList[secondCharacterAsInt - 1], chemicalType);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("이미지 로드 중 오류 발생: " + ex.Message);
+            }
+        }
+        private void SetPictureBoxImage(PictureBox pictureBox, string chemicalType)
+        {
+            switch (chemicalType)
+            {
+                case "인산(H3PO4)":
+                    pictureBox.Image = Properties.Resources.tankak4;
+                    break;
+                case "암모니아(NH4OH)":
+                    pictureBox.Image = Properties.Resources.tankam4;
+                    break;
+                case "황산(H2SO4)":
+                    pictureBox.Image = Properties.Resources.tankbs4;
+                    break;
+                case "과산화수소(H2O2)":
+                    pictureBox.Image = Properties.Resources.tankgs4;
+                    break;
+                case "불산(HF)":
+                    pictureBox.Image = Properties.Resources.tankhs4;
+                    break;
+                case "질산(NHO3)":
+                    pictureBox.Image = Properties.Resources.tankis4;
+                    break;
+                case "알코올(Iso Propyl Alchol)":
+                case "알카리":
+                    pictureBox.Image = Properties.Resources.tankjs4;
+                    break;
+                case "염산(HCl)":
+                case "염산":
+                    pictureBox.Image = Properties.Resources.tankys4;
+                    break;
+                    // 다른 경우에 대한 처리도 추가 가능
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            LoadImageFromDatabase();
         }
     }
 }
